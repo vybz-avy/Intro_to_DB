7ee9c2b465f25176aebe10628c2e53967215e01d
vybz-avy <nizeyealvin@gmail.com>
2025-08-15 15:57:26 +0300

    Add files via upload

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+-- Select the database
+USE alx_book_store;
+-- List all tables in the current database
+SHOW TABLES;
+mysql -u your_username -p -D alx_book_store < path/to/task_3.sql
+SHOW TABLES;
+mysql -u your_username -p -D your_database_name < path/to/task_3.sql
