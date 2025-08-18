7afd4d10bfd4c30cd01cc4cf9815d542837dc98b
vybz-avy <nizeyealvin@gmail.com>
2025-08-18 12:29:18 +0300

    Add files via upload

---
+++ task_6.sql
@@ -0,0 +1,10 @@
+-- task_6.sql
+
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+  (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+  (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+  (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
+  mysql -D your_database_name -e "$(cat task_6.sql)"
+  mysql -D database_name -e "$(cat task_6.sql)"
